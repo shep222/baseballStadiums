3b4e88796f2ed7d7d00b2c0c84f6cdbc9035a45d
John Shepler <john.sheplerjr@gmail.com>
2017-02-06 17:38:39 -0700

    update pin drop

--- public/maps.js
+++ public/maps.js
@@ -1,14 +1,16 @@
 function initMap() {
     var uluru = {
-        lat: 37.09024,
-        lng: -95.712891
+        lat: 41.8781136,
+        lng: -87.6297982
     };
     var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 4,
+        zoom: 5,
         center: uluru
     });
     var marker = new google.maps.Marker({
         position: uluru,
+        icon: "http://mlb.com/assets/images/2/0/2/111260202/cuts/CHC_1x_w6j9ueiq_8gdet1h6.png",
+
         map: map
     });
 }
