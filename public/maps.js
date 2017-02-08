307c3654bcb1b674d8c33c46600dcbf0ff75d9c1
John Shepler <john.sheplerjr@gmail.com>
2017-02-08 14:41:22 -0700

    place logos in map

--- public/maps.js
+++ public/maps.js
@@ -1,40 +1,38 @@
-var stadium0 = ["Arizona", "Diamondbacks", "Chase Field", "'mlb.com/assets/images/1/7/0/152690170/cuts/ARI_1x_wt0zf7zj_jsvgt65k.png'", "NL", "NL-W"];
-var stadium1 = ["Atlanta", "Braves", "Turner Field", "mlb.com/assets/images/3/0/4/113367304/cuts/ATL_light_1x_y6siew5x_s51kx8q3.png", "NL", "NL-E"];
-var stadium2 = ["Baltimore", "Orioles", "Oriole Park at Camden Yards", "mlb.com/assets/images/4/4/0/111261440/cuts/BAL_1x_5dk2mwfk_hr6p7zsm.png", "AL", "AL-E"];
-var stadium3 = ["Boston", "Red Sox", "Fenway Park", "mlb.com/assets/images/8/1/0/111260810/cuts/BOS_1x_omwx1j3o_7o1v91an.png", "AL", "AL-E"];
-var stadium4 = ["Chicago", "Cubs", "Wrigley Field", "mlb.com/assets/images/2/0/2/111260202/cuts/CHC_1x_w6j9ueiq_8gdet1h6.png", "NL", "NL-C"];
-var stadium5 = ["Chicago", "WhiteSocks", "U.S. Cellular Field", "mlb.com/assets/images/0/2/8/111260028/cuts/CWS_1x_ngpow266_slkjjutl.png", "AL", "AL-C"];
-var stadium6 = ["Cincinnati", "Reds", "Great American Ball Park", "mlb.com/assets/images/9/9/6/111258996/cuts/CIN_1x_z3lf2pg7_ao2jmblu.png", "NL", "NL-C"];
-var stadium7 = ["Cleveland", "Indians", "Progressive Field (Jacobs Field)", "mlb.com/assets/images/2/8/8/111258288/cuts/CLE_1x_s5vl5xsu_bxdr7zl6.png", "AL", "AL-C"];
-var stadium8 = ["Colorado", "Rockies", "Coors Field", "mlb.com/assets/images/0/0/8/111257008/cuts/COL_1x_veqnjq6e_e2r25mj8.png", "NL", "NL-W"];
-var stadium9 = ["Detroit", "Tigers", "Comerica Park", "mlb.com/assets/images/8/2/0/111256820/cuts/DET_1x_uisn11sj_h61wu3p5.png", "AL", "AL-C"];
-var stadium10 = ["Houston", "Astros", "Minute Maid Park", "mlb.com/assets/images/3/7/4/111253374/cuts/HOU_1x_52x0czes_0c7icp6d.png", "NL", "AL-W"];
-var stadium11 = ["Kansas City", "Royals", "Kauffman Stadium", "mlb.com/assets/images/7/3/4/111248734/cuts/KC_1x_65ouhrfa_h000z1pp.png", "AL", "AL-C"];
-var stadium12 = ["LA", "Angels", "Angel Stadium of Anaheim", "mlb.com/assets/images/8/7/0/111247870/cuts/LAA_1x_itp0lezg_xwicudxb.png", "AL", "AL-W"];
-var stadium13 = ["LA", "Dodgers", "Dodger Stadium", "mlb.com/assets/images/4/1/0/111246410/cuts/LAD_1x_jlbsdlwr_ke639ch6.png", "NL", "NL-W"];
-var stadium14 = ["Miami", "Marlins", "Marlins Park", "mlb.com/assets/images/8/8/0/111244880/cuts/MIA_1x_bqpvqxvb_wt8pft8m.png", "NL", "NL-E"];
-var stadium15 = ["Milwaukee", "Brewers", "Miller Park", "mlb.com/assets/images/4/3/4/111242434/cuts/MIL_1x_22esvruq_dv2ldcgq.png", "NL", "NL-C"];
-var stadium16 = ["Minnesota", "Twins", "Metrodome", "mlb.com/assets/images/5/9/2/111241592/cuts/MIN_1x_k9i7jf81_w9f0bsfc.png", "AL", "AL-C"];
-var stadium17 = ["NY", "Mets", "Shae Stadium", "mlb.com/assets/images/0/9/4/111239094/cuts/NYM_1x_hyxeqsxx_1r0ymvgt.png", "NL", "NL-E"];
-var stadium18 = ["NY", "Yankees", "Yankee Stadium", "mlb.com/assets/images/2/7/8/111220278/cuts/NYY_1x_l2i2du2h_0e42q27s.png", "AL", "AL-E"];
-var stadium19 = ["Oakland", "Athletics", "McAfee Coliseum", "mlb.com/assets/images/4/8/4/111237484/cuts/OAK_1x_8rx9ve6u_jzsaolao.png", "AL", "AL-W"];
-var stadium20 = ["Philadelphia", "Phillies", "Citizens Bank Park", "mlb.com/assets/images/6/8/0/111236680/cuts/PHI_1x_2h9yfq7s_whh91edb.png", "NL", "NL-E"];
-var stadium21 = ["Pittsburgh", "Pirates", "PNC Park", "mlb.com/assets/images/8/8/0/165759880/cuts/pit_masthead_2016_px6hwdo4_517zg5ze.png", "NL", "NL-C"];
-var stadium22 = ["San Diego", "Padres", "PETCO Park", "mlb.com/assets/images/8/1/4/111219814/cuts/SD_1x_7j83z8a3_h30ln72b.png", "NL", "NL-W"];
-var stadium23 = ["San Francisco", "Giants", "AT&T Park", "mlb.com/assets/images/3/5/4/111217354/cuts/SF_1x_9zr4042i_iwyin6qv.png", "NL", "NL-W"];
-var stadium24 = ["Seattle", "Mariners", "Safeco Field", "mbl.com/assets/images/1/4/4/111216144/cuts/SEA_1x_gcdpf363_aiurokko.png", "AL", "AL-W"];
-var stadium25 = ["St Louis", "Cardinals", "Busch Stadium", "mlb.com/assets/images/3/7/8/111214378/cuts/STL_1x_1pa5x162_qehvt8mx.png", "NL", "NL-C"];
-var stadium26 = ["Tampa Bay", "Rays", "Tropicana Field", "mlb.com/assets/images/1/3/8/111213138/cuts/TB_1x_4xko8mzj_q4d8rga8.png", "AL", "AL-E"];
-var stadium27 = ["Texas", "Rangers", "Rangers Ballpark in Arlington", "mlb.com/assets/images/2/0/6/111282206/cuts/TEX_1x_ivqvhhp7_dx4khjd5.png", "AL", "AL-W"];
-var stadium28 = ["Toronto", "Blue Jays", "Rogers Centre", "mlb.com/assets/images/7/2/8/111198728/cuts/TOR_1x_einmv0ce_nwsb3ka2.png", "AL", "AL-E"];
-var stadium29 = ["Washington", "Nationals", "Nationals Park", "mlb.com/assets/images/6/8/0/111198680/cuts/WAS_1x_t4xwwaet_ssq428qw.png", "NL", "NL-E"];
+var stadium0 = ["Arizona", "Diamondbacks", "Chase Field", "http://mlb.com/assets/images/1/7/0/152690170/cuts/ARI_1x_wt0zf7zj_jsvgt65k.png", "NL", "NL-W"];
+var stadium1 = ["Atlanta", "Braves", "Turner Field", "http://mlb.com/assets/images/3/0/4/113367304/cuts/ATL_light_1x_y6siew5x_s51kx8q3.png", "NL", "NL-E"];
+var stadium2 = ["Baltimore", "Orioles", "Oriole Park at Camden Yards", "http://mlb.com/assets/images/4/4/0/111261440/cuts/BAL_1x_5dk2mwfk_hr6p7zsm.png", "AL", "AL-E"];
+var stadium3 = ["Boston", "Red Sox", "Fenway Park", "http://mlb.com/assets/images/8/1/0/111260810/cuts/BOS_1x_omwx1j3o_7o1v91an.png", "AL", "AL-E"];
+var stadium4 = ["Chicago", "Cubs", "Wrigley Field", "http://mlb.com/assets/images/2/0/2/111260202/cuts/CHC_1x_w6j9ueiq_8gdet1h6.png", "NL", "NL-C"];
+var stadium5 = ["Chicago", "WhiteSocks", "U.S. Cellular Field", "http://mlb.com/assets/images/0/2/8/111260028/cuts/CWS_1x_ngpow266_slkjjutl.png", "AL", "AL-C"];
+var stadium6 = ["Cincinnati", "Reds", "Great American Ball Park", "http://mlb.com/assets/images/9/9/6/111258996/cuts/CIN_1x_z3lf2pg7_ao2jmblu.png", "NL", "NL-C"];
+var stadium7 = ["Cleveland", "Indians", "Progressive Field (Jacobs Field)", "http://mlb.com/assets/images/2/8/8/111258288/cuts/CLE_1x_s5vl5xsu_bxdr7zl6.png", "AL", "AL-C"];
+var stadium8 = ["Colorado", "Rockies", "Coors Field", "http://mlb.com/assets/images/0/0/8/111257008/cuts/COL_1x_veqnjq6e_e2r25mj8.png", "NL", "NL-W"];
+var stadium9 = ["Detroit", "Tigers", "Comerica Park", "http://mlb.com/assets/images/8/2/0/111256820/cuts/DET_1x_uisn11sj_h61wu3p5.png", "AL", "AL-C"];
+var stadium10 = ["Houston", "Astros", "Minute Maid Park", "http://mlb.com/assets/images/3/7/4/111253374/cuts/HOU_1x_52x0czes_0c7icp6d.png", "NL", "AL-W"];
+var stadium11 = ["Kansas City", "Royals", "Kauffman Stadium", "http://mlb.com/assets/images/7/3/4/111248734/cuts/KC_1x_65ouhrfa_h000z1pp.png", "AL", "AL-C"];
+var stadium12 = ["LA", "Angels", "Angel Stadium of Anaheim", "http://mlb.com/assets/images/8/7/0/111247870/cuts/LAA_1x_itp0lezg_xwicudxb.png", "AL", "AL-W"];
+var stadium13 = ["LA", "Dodgers", "Dodger Stadium", "http://mlb.com/assets/images/4/1/0/111246410/cuts/LAD_1x_jlbsdlwr_ke639ch6.png", "NL", "NL-W"];
+var stadium14 = ["Miami", "Marlins", "Marlins Park", "http://mlb.com/assets/images/8/8/0/111244880/cuts/MIA_1x_bqpvqxvb_wt8pft8m.png", "NL", "NL-E"];
+var stadium15 = ["Milwaukee", "Brewers", "Miller Park", "http://mlb.com/assets/images/4/3/4/111242434/cuts/MIL_1x_22esvruq_dv2ldcgq.png", "NL", "NL-C"];
+var stadium16 = ["Minnesota", "Twins", "Metrodome", "http://mlb.com/assets/images/5/9/2/111241592/cuts/MIN_1x_k9i7jf81_w9f0bsfc.png", "AL", "AL-C"];
+var stadium17 = ["NY", "Mets", "Shae Stadium", "http://mlb.com/assets/images/0/9/4/111239094/cuts/NYM_1x_hyxeqsxx_1r0ymvgt.png", "NL", "NL-E"];
+var stadium18 = ["NY", "Yankees", "Yankee Stadium", "http://mlb.com/assets/images/2/7/8/111220278/cuts/NYY_1x_l2i2du2h_0e42q27s.png", "AL", "AL-E"];
+var stadium19 = ["Oakland", "Athletics", "McAfee Coliseum", "http://mlb.com/assets/images/4/8/4/111237484/cuts/OAK_1x_8rx9ve6u_jzsaolao.png", "AL", "AL-W"];
+var stadium20 = ["Philadelphia", "Phillies", "Citizens Bank Park", "http://mlb.com/assets/images/6/8/0/111236680/cuts/PHI_1x_2h9yfq7s_whh91edb.png", "NL", "NL-E"];
+var stadium21 = ["Pittsburgh", "Pirates", "PNC Park", "http://mlb.com/assets/images/8/8/0/165759880/cuts/pit_masthead_2016_px6hwdo4_517zg5ze.png", "NL", "NL-C"];
+var stadium22 = ["San Diego", "Padres", "PETCO Park", "http://mlb.com/assets/images/8/1/4/111219814/cuts/SD_1x_7j83z8a3_h30ln72b.png", "NL", "NL-W"];
+var stadium23 = ["San Francisco", "Giants", "AT&T Park", "http://mlb.com/assets/images/3/5/4/111217354/cuts/SF_1x_9zr4042i_iwyin6qv.png", "NL", "NL-W"];
+var stadium24 = ["Seattle", "Mariners", "Safeco Field", "http://mbl.com/assets/images/1/4/4/111216144/cuts/SEA_1x_gcdpf363_aiurokko.png", "AL", "AL-W"];
+var stadium25 = ["St Louis", "Cardinals", "Busch Stadium", "http://mlb.com/assets/images/3/7/8/111214378/cuts/STL_1x_1pa5x162_qehvt8mx.png", "NL", "NL-C"];
+var stadium26 = ["Tampa Bay", "Rays", "Tropicana Field", "http://mlb.com/assets/images/1/3/8/111213138/cuts/TB_1x_4xko8mzj_q4d8rga8.png", "AL", "AL-E"];
+var stadium27 = ["Texas", "Rangers", "Rangers Ballpark in Arlington", "http://mlb.com/assets/images/2/0/6/111282206/cuts/TEX_1x_ivqvhhp7_dx4khjd5.png", "AL", "AL-W"];
+var stadium28 = ["Toronto", "Blue Jays", "Rogers Centre", "http://mlb.com/assets/images/7/2/8/111198728/cuts/TOR_1x_einmv0ce_nwsb3ka2.png", "AL", "AL-E"];
+var stadium29 = ["Washington", "Nationals", "Nationals Park", "http://mlb.com/assets/images/6/8/0/111198680/cuts/WAS_1x_t4xwwaet_ssq428qw.png", "NL", "NL-E"];
 var stadiums = [stadium0, stadium1, stadium2, stadium3, stadium4, stadium5, stadium6, stadium7, stadium8, stadium9,
     stadium10, stadium11, stadium12, stadium13, stadium14, stadium15, stadium16, stadium17, stadium18, stadium19,
     stadium20, stadium21, stadium22, stadium23, stadium24, stadium25, stadium26, stadium27, stadium28, stadium29
 ];
 
-
-var myStadiums = [];
 $.ajax({
     url: 'https://api.stattleship.com/baseball/mlb/teams',
     headers: {
@@ -45,28 +43,38 @@ $.ajax({
     method: 'GET',
     dataType: 'json'
 }).then(data);
+var myStadiums = [];
 
 function data(teams) {
     for (i = 0; i < teams.teams.length; i++) {
         myStadiums.push([stadiums[i][0], stadiums[i][1], stadiums[i][2], stadiums[i][3], stadiums[i][4], stadiums[i][5], teams.teams[i].latitude, teams.teams[i].longitude]);
     }
-    console.log(myStadiums);
-
+    initMap(myStadiums);
 }
 
-
-function initMap() {
-    var uluru = {
-        lat: 41.8781136,
-        lng: -87.6297982
-    };
+function initMap(myStadiums) {
     var map = new google.maps.Map(document.getElementById('map'), {
         zoom: 4,
-        center: uluru
+        center: new google.maps.LatLng(37.09024, -95.712891)
     });
-    var marker = new google.maps.Marker({
-        position: uluru,
-        icon: "http://mlb.com/assets/images/2/0/2/111260202/cuts/CHC_1x_w6j9ueiq_8gdet1h6.png",
+    function addMarker(eachStadium){
+      var marker = new google.maps.Marker({
+        position: eachStadium.position,
+        icon: eachStadium.icon,
         map: map
-    });
+      });
+    }
+
+    var eachStadium = [];
+    for(i=0;i<myStadiums.length;i++){
+      eachStadium.push({position: new google.maps.LatLng(myStadiums[i][6],myStadiums[i][7]),
+      icon: myStadiums[i][3]});
+}
+
+    console.log(eachStadium);
+    for (j=0;j<eachStadium.length;j++){
+      addMarker(eachStadium[j]);
+    }
+
+      
 }
