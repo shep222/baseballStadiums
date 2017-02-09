5e88d3fc6cf2473940b0acca902515fce4374994
John Shepler <john.sheplerjr@gmail.com>
2017-02-09 16:06:55 -0700

    add sort and filter but logos are wrong at filter

--- public/maps.js
+++ public/maps.js
@@ -22,35 +22,58 @@ function initMap(newStadiums) {
     var infoWindow = new google.maps.InfoWindow({
 
     });
-    function addMarker(eachStadium) {
+    var markers = [];
+    function addMarker(eachStadium, i) {
         var marker = new google.maps.Marker({
-            position: eachStadium.position,
-            icon: eachStadium.icon,
-            map: map,
-
-        });
-        marker.addListener('click', function() {
-              infoWindow.setContent(eachStadium.teamName  + "<br>" + eachStadium.stadiumName);
-              infoWindow.open(map, marker);
-            });
-    }
-    var eachStadium = [];
-    for (i = 0; i < newStadiums.length; i++) {
-        eachStadium.push({
-            position: new google.maps.LatLng(newStadiums[i].latitude, newStadiums[i].longitude),
+            position: new google.maps.LatLng(eachStadium.latitude, eachStadium.longitude),
             icon: {
                 url: "images/logos.png",
                 size: new google.maps.Size(32, 32),
                 origin: new google.maps.Point(32, 32 * (i + 1)),
                 scaledSize: new google.maps.Size(64, 1152)
             },
-            stadiumName: newStadiums[i].stadiumName,
-            teamName: newStadiums[i].teamName
+
+            map: map,
+
         });
+        markers.push(marker);
+        marker.addListener('click', function() {
+              infoWindow.setContent(eachStadium.teamName  + "<br>" + eachStadium.stadiumName);
+              infoWindow.open(map, marker);
+            });
     }
-    for (j = 0; j < eachStadium.length; j++) {
-        addMarker(eachStadium[j]);
+
+
+    for (j = 0; j < newStadiums.length; j++) {
+        addMarker(newStadiums[j],j);
     }
+
+    $('.dropdownSort').on('change', function(){
+      markers.forEach(function(marker){
+        marker.setMap(null);
+      });
+      var mySortDecision = $('.dropdownSort').val();
+      if (mySortDecision=== "National League"){
+        var nlTeams = newStadiums.filter(function(team){
+          return team.league === "NL";
+        });
+        for (j = 0; j < nlTeams.length; j++) {
+            addMarker(nlTeams[j],j);
+          }
+      } else if (mySortDecision=== "American League"){
+        var alTeams = newStadiums.filter(function(team){
+          return team.league === "AL";
+        });
+        for (j = 0; j < alTeams.length; j++) {
+            addMarker(alTeams[j],j);
+          }
+      } else {
+        for (j = 0; j < newStadiums.length; j++) {
+            addMarker(newStadiums[j],j);
+          }
+      }
+
+    });
 
 
 
